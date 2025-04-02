7c138f94852e1fe19ad4c2f8137bb9ebb7d910a7
YevhenK18 <yevheniikaliush@gmail.com>
2025-04-02 11:09:45 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Импорт Bootstrap
-import './index.css'; // Оставляем для кастомных стилей (если нужно)
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './index.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
